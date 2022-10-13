ea0dbb6ad471dd9bf1498cbddbf6da37b28b6491
Espen Moe-Nilssen <espen@medialog.no>
2022-10-13 18:44:31 +0200

    Update rename.py

--- src/medialog/rename/portlets/rename.py
+++ src/medialog/rename/portlets/rename.py
@@ -18,6 +18,12 @@ import re
 
 
 class IRenamePortlet(IPortletDataProvider):
+
+    enable = schema.Bool(
+        title=_(u'Enable replacing'),
+        required=False,
+    )
+
     find_str = schema.TextLine(
         title=_(u'Expression (find)'),
         description=_(u'Change (in) image names from'),  # NOQA: E501
@@ -32,6 +38,25 @@ class IRenamePortlet(IPortletDataProvider):
         default=u''
     )
 
+    period = schema.Bool(
+        title=_(u'period to space'),
+        required=False,
+    )
+
+    hyphen= schema.Bool(
+        title=_(u'hyphen to space'),
+        required=False,
+    )
+
+    star = schema.Bool(
+        title=_(u'* to space'),
+        required=False,
+    )
+
+    yeer = schema.Bool(
+        title=_(u'Add year to field year. Dont enable this'),
+        required=False,
+    )
 
 @implementer(IRenamePortlet)
 class Assignment(base.Assignment):
@@ -95,36 +120,64 @@ class Renderer(base.Renderer):
     #    return self.data.find_str
 
 
+
     def rename(self):
-        import pdb; pdb.set_trace()
-        find = self.data.find_str
-        replace = self.data.replace_str
 
-        folder_path = self.folder_path()
+        if self.data.enable:
+            find = self.data.find_str
+            replace = self.data.replace_str
 
-        all_items = self.context.portal_catalog(portal_type='Image', path={'query': folder_path,})
+            folder_path = self.folder_path()
+            #folder_path = '/'.join(context.getPhysicalPath())
+            folder_path = '/'.join(folder_path)
 
-        for my_image in all_items:
-            title = my_image.Title
-            #print(title)
-            new_title = title.replace(find, replace)
-            new_title = new_title.replace('.', ' ')
-            new_title = new_title.replace('-', ' ')
-            new_title = new_title.replace('*', '')
+            all_items = self.context.portal_catalog(portal_type='Image', path={'query': folder_path,})
+
+            for my_image in all_items:
+                title = my_image.Title
+                #print(title)
+                new_title = title.replace(find, replace)
+
+                if self.data.period:
+                    new_title = new_title.replace('.', ' ')
+
+                if self.data.hyphen:
+                    new_title = new_title.replace('-', ' ')
+
+                if self.data.star:
+                    new_title = new_title.replace('*', '')
 
 
-            title_len = len(new_title.split(" "))
-            if (title_len > 1):
-                first_word = new_title.split(" ")[0];
-                if first_word[0].isdigit():
-                    new_title = new_title.split(" ")[1] + " " + first_word 
+                title_len = len(new_title.split(" "))
+                #import pdb; pdb.set_trace()
 
-            #new_title.replace(a, b)
-            #print(new_title)
+                if (title_len > 1):
+                    first_word = new_title.split(" ")[0];
+                    last_part = new_title.split(" ")[1:]
+                    if first_word[0].isdigit():
+                        ##obs need to add spaces
+                        new_title = ' '.join(last_part) + " " + first_word
 
-            item = my_image.getObject()
+                    # take year from title and add it to field 'year'
+                    if self.data.yeer:
+                        for word in last_part:
+                            print(word)
+                            if word[0] == "(" and word[-1] == ")":
+                                print('will split')
+                                word = word[1:-1]
+                        print(word)
+                        if word.isdigit():
+                            if int(word) > 1700 and int(word) < 2040:
+                                item.year = int(word)
 
-            item.setTitle(new_title)
-            transaction.get().commit()
+                #new_title.replace(a, b)
+                #print(new_title)
 
-        return 'You need to rebuild catalog now.'
+                item = my_image.getObject()
+
+                item.setTitle(new_title)
+                transaction.get().commit()
+
+            return 'You need to rebuild catalog now.'
+
+        return 'Portlet is disabled'
